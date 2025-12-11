6cede549471d1f53738d622040878b909c048c13
m1890 <m1890@users.noreply.github.com>
2025-12-11 18:38:05 +0800

    Bump version to 1.3.0 - improved app launch

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.autoglm.helper"
         minSdk = 24  // Android 7.0
         targetSdk = 34
-        versionCode = 3
-        versionName = "1.2.0"
+        versionCode = 4
+        versionName = "1.3.0"
     }
 
     buildTypes {
